cf7fc1f08fd71e9a4267173bb4da79d89afc1faa
Roger Pence <rp@rogerpence.com>
2024-06-28 00:24:37 -0500

    add some jsdoc stuff

--- src/lib/scripts/types.js
+++ src/lib/scripts/types.js
@@ -8,15 +8,8 @@
  */
 
 /**
- * @typedef {Object} WritableObjectStore
- * @property {function(Object):void} set - Sets the value of the store.
- * @property {function(function(Object):Object):void} update - Updates the value of the store based on a callback function.
- * @property {function(function(Object):void):function():void} subscribe - Subscribes to changes in the store value.
+ * @typedef {any} WritableObjectStore
+ * @property {function(any):void} set - Sets the value of the store.
+ * @property {function(function(any):any):void} update - Updates the value of the store based on a callback function.
+ * @property {function(function(any):void):function():void} subscribe - Subscribes to changes in the store value.
  */
-
-// /**
-//  * @typedef {Object} WritableObjectStore
-//  * @property {function(Object):void} set - Sets the value of the store.
-//  * @property {function(function(Object):Object):void} update - Updates the value of the store based on a callback function.
-//  * @property {function(function(Object):void):function():void} subscribe - Subscribes to changes in the store value.
-//  */
